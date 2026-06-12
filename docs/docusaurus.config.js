0a15520909450a14dba7c833f5bfa97307e59de9
Jason Vranek <jasonvranek@gmail.com>
2026-06-11 17:08:05 -0700

    edit url for testing on fork

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://commit-boost.github.io',
+  url: 'https://JasonVranek.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/commit-boost-client/',
